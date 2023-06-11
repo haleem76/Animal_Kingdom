bc88da418da2292c5235af16cad82078df498e20
Halim Aminnudin <111233808+haleem76@users.noreply.github.com>
2023-06-11 16:43:39 +0800

    upload
    upload

---
+++ NinjaCat.java
@@ -0,0 +1,44 @@
+import java.awt.*;
+
+public class NinjaCat extends Tiger
+{
+
+    public boolean hasInfected;
+
+    public NinjaCat ()
+    {
+        hasInfected=false;
+    }
+
+    public Color getColor()
+    {
+        if (hasInfected){
+            return Color.MAGENTA;
+        } else {
+            return Color.orange;
+        }
+    }
+
+
+    public String toString()
+    {
+        if (hasInfected)
+        {
+            return "Cat-N";
+        } else
+        {
+            return "N-Cat";
+        }
+    }
+
+
+    public Action getMove(CritterInfo info)
+    {
+        //same as Tiger, but changes color when has infected
+        if (info.getFront()==Neighbor.OTHER)
+        {
+            hasInfected=true;
+        }
+        return super.getMove(info);
+    }
+}
